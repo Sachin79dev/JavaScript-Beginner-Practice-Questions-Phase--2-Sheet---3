c19f25ff00d425553f361786a83936b735b72705
Sachin Singh <90830959+Sachin79dev@users.noreply.github.com>
2026-06-01 18:51:19 +0530

    Add student pass/fail evaluation logic
    // 2. You are given an array of students.
    // "Pass" if marks are greater than 50
    // "Fail" otherwise

--- app.js
+++ app.js
@@ -8,3 +8,25 @@ let prices = [100, 250, 399, 499];
 prices.forEach((n) => {
     console.log(`₹${n}`); 
 })
+
+
+
+// 2. You are given an array of students.
+// "Pass" if marks are greater than 50
+// "Fail" otherwise
+
+
+let students = [
+  { name: "Anubhav", marks: 85 },
+  { name: "Rahul", marks: 42 },
+  { name: "Aman", marks: 90 },
+];
+
+
+students.forEach((n) => {
+    if(n.marks > 50) {
+        console.log(`${n.name} - Pass`);
+    } else {
+        console.log(`${n.name} - Fail`);
+    }
+})
